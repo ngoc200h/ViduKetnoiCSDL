37f297823d589540448172814953567b45dea148
Hải Ngọc <94596333+ngoc200h@users.noreply.github.com>
2022-05-29 12:32:32 +0700

    Update frmChatLieu.cs

--- frmChatLieu.cs
+++ frmChatLieu.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ViduKetnoiCSDL.Class;
 
 namespace ViduKetnoiCSDL
 {
@@ -20,19 +21,21 @@ namespace ViduKetnoiCSDL
             InitializeComponent();
         }
 
-        Database dt = new Database();
-
+        DataTable tblCL;
 
         private void btnThem_Click(object sender, EventArgs e)
         {
-            Reset();
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            btnBoQua.Enabled = true;
             btnLuu.Enabled = true;
-            btnSua.Enabled = true;
-            btnXoa.Enabled = true;
+            btnThem.Enabled = false;
+            ResetValues();
+            txtMaChatLieu.Enabled = true;
             txtMaChatLieu.Focus();
         }
 
-        private void Reset()
+        private void ResetValues()
         {
             txtMaChatLieu.Text = "";
             txtTenChatLieu.Text = "";
@@ -40,24 +43,24 @@ namespace ViduKetnoiCSDL
 
         private void btnXoa_Click(object sender, EventArgs e) //phải luôn có câu hỏi 'bạn có muốn xóa ko?'
         {
-
-            if (MessageBox.Show("Bạn có chắc chắn xóa  " + txtMaChatLieu.Text 
-                + " không? Nếu có ấn nút Lưu, không thì ấn nút Hủy", "Xóa sản phẩm", 
-                MessageBoxButtons.YesNo) == DialogResult.Yes)
+            string sql;
+            if (tblCL.Rows.Count == 0)
             {
-                try
-                {
-                    string sql = "delete from ChatLieu where MaChatLieu='" + txtMaChatLieu.Text + "'";
-                    dt.ExcuteNonQuery(sql);
-                    string sql1 = "select * from ChatLieu";
-                    dgvChatLieu.DataSource = dt.LoadDataToTable(sql1);
-                }
-                catch(Exception ex)
-                {
-                    MessageBox.Show("Xóa ko nổi.. " + ex.ToString());
-                }
+                MessageBox.Show("Không còn dữ liệu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
             }
-
+            if (txtMaChatLieu.Text == "")
+            {
+                MessageBox.Show("Bạn chưa chọn bản ghi nào", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (MessageBox.Show("Bạn có muốn xóa không?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                sql = "DELETE ChatLieu WHERE MaChatLieu=N'" + txtMaChatLieu.Text + "'";
+                Class.Database.RunSqlDel(sql);
+                LoadDMChatLieu();
+                ResetValues();
+            }
         }
 
         int dong;
@@ -69,22 +72,52 @@ namespace ViduKetnoiCSDL
         }
 
 
-
         private void btnSua_Click(object sender, EventArgs e)
         {
-            string sql = "update ChatLieu set TenChatLieu='" + txtTenChatLieu.Text + "' where MaChatLieu='" + txtMaChatLieu.Text + "'";
-            dt.ExcuteNonQuery(sql);
+            //string sql = "update ChatLieu set TenChatLieu='" + txtTenChatLieu.Text + "' where MaChatLieu='" + txtMaChatLieu.Text + "'";
+            //dt.ExcuteNonQuery(sql);
+            //LoadDMChatLieu();
+            string sql;
+            if (tblCL.Rows.Count == 0)
+            {
+                MessageBox.Show("Không còn dữ liệu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (txtMaChatLieu.Text == "")
+            {
+                MessageBox.Show("Bạn chưa chọn bản ghi nào", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (txtTenChatLieu.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Bạn phải nhập tên chất liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtTenChatLieu.Focus();
+                return;
+            }
+            sql = "UPDATE ChatLieu SET TenChatLieu=N'" + txtTenChatLieu.Text.ToString() + "' WHERE MaChatLieu=N'" + txtMaChatLieu.Text + "'";
+            Class.Database.RunSql(sql);
             LoadDMChatLieu();
+            ResetValues();
+            btnBoQua.Enabled = false;
+
         }
 
-        private void LoadDMChatLieu()
+        private void LoadDMChatLieu() //Load_DataGridView()
         {
-            string sql1 = "select * from ChatLieu";
-            dgvChatLieu.DataSource = dt.LoadDataToTable(sql1);
+            string sql = "select * from ChatLieu";
+            tblCL = Class.Database.LoadDataToTable(sql);
+            dgvChatLieu.DataSource = tblCL;
+            // Không cho phép thêm mới dữ liệu trực tiếp trên lưới
+            dgvChatLieu.AllowUserToAddRows = false;
+            // Không cho phép sửa dữ liệu trực tiếp trên lưới
+            dgvChatLieu.EditMode = DataGridViewEditMode.EditProgrammatically;
         }
 
         private void frmChatLieu_Load(object sender, EventArgs e)
         {
+            txtMaChatLieu.Enabled = false;
+            btnLuu.Enabled = false;
+            btnBoQua.Enabled = false;
             LoadDMChatLieu();
             //string sql1 = "select * from ChatLieu";
             //dgvChatLieu.DataSource = dt.TaoBang(sql1);
@@ -98,47 +131,63 @@ namespace ViduKetnoiCSDL
             txtTenChatLieu.Text = Convert.ToString(row.Cells["tenchatlieu"].Value);
         }
 
-        private void btnCapNhat_Click(object sender, EventArgs e)
-        {
-            Reset();
-        }
-
         private void btnLuu_Click(object sender, EventArgs e)
         {
+            string sql;
             //check dữ liệu ko dc để trống
-            //if (txtMaChatLieu.Text.Trim() == "")
-            //{
-            //    MessageBox.Show("Mã Chất liệu ko đc để trống!");
-            //    txtMaChatLieu.Focus();
-            //    return;
-            //}
-            //if (txtTenChatLieu.Text.Trim() == "")
-            //{
-            //    MessageBox.Show("Ten Chất liệu ko đc để trống!");
-            //    txtTenChatLieu.Focus();
-            //    return;
-            //}
-
-            //Check xem có bị trùng key ko
-            //string sql1 = "select * from ChatLieu where MaChatLieu = '"
-            //            + txtMaChatLieu.Text + "'";
-
-            string sql = "insert into ChatLieu values('" + txtMaChatLieu.Text + "','" + txtTenChatLieu.Text + "')";
-            try
+            if (txtMaChatLieu.Text.Trim() == "")
             {
-                dt.ExcuteNonQuery(sql);
-                btnLuu.Enabled = false;
-                btnXoa.Enabled = true;
-                btnSua.Enabled = true;
-                btnCapNhat.Enabled = false;
+                MessageBox.Show("Mã Chất liệu ko đc để trống!");
+                txtMaChatLieu.Focus();
+                return;
             }
-            catch
+            if (txtTenChatLieu.Text.Trim() == "")
             {
-                MessageBox.Show("Đã tồn tại mã chất liệu", "Úi có lỗi");
+                MessageBox.Show("Ten Chất liệu ko đc để trống!");
+                txtTenChatLieu.Focus();
+                return;
             }
-            string sql1 = "select * from ChatLieu";
-            dgvChatLieu.DataSource = dt.LoadDataToTable(sql1);
-            //LoadDMChatLieu();
+            sql = "SELECT MaChatlieu FROM ChatLieu WHERE MaChatlieu=N'" +
+            txtMaChatLieu.Text.Trim() + "'";
+            if (Class.Database.CheckKey(sql))
+            {
+                MessageBox.Show("Mã chất liệu này đã có, bạn phải nhập mã khác", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtMaChatLieu.Focus();
+                txtMaChatLieu.Text = "";
+                return;
+            }
+            sql = "INSERT INTO ChatLieu(MaChatlieu,TenChatLieu) VALUES(N'" + txtMaChatLieu.Text + "',N'" + txtTenChatLieu.Text + "')";
+            Class.Database.RunSql(sql);
+            LoadDMChatLieu();
+            ResetValues();
+            btnXoa.Enabled = true;
+            btnThem.Enabled = true;
+            btnSua.Enabled = true;
+            btnBoQua.Enabled = false;
+            btnLuu.Enabled = false;
+            txtMaChatLieu.Enabled = false;
+
+        }
+        private void btnBoQua_Click(object sender, EventArgs e)
+        {
+            ResetValues();
+            btnBoQua.Enabled = false;
+            btnThem.Enabled = true;
+            btnXoa.Enabled = true;
+            btnSua.Enabled = true;
+            btnLuu.Enabled = false;
+            txtMaChatLieu.Enabled = false;
+        }
+
+        private void txtMaChatLieu_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                SendKeys.Send("{TAB}");
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
